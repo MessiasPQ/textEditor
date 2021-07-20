67b96ec5ec1dd303bdc3d061e47dd69526dfae05
Messias Queiroz <messias1572@hotmail.com>
2021-07-20 07:39:19 -0300

    Criando menu do editor de texto

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TextEditor");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha uma das opções abaixo:");
+            Console.WriteLine("1- Abrir / 2- Criar novo arquivo / 0- Sair");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+
+        }
+
+        static void Editar()
+        {
+
         }
     }
 }
